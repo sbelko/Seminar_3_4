a31a11581f97dfcae47136818e9d95cfa6041156
sbelko <sbelko@mail.ru>
2023-02-09 10:11:10 +0300

    Homework23

---
+++ Homework23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.Write("Введите число: "); int n = int.Parse(Console.ReadLine()!);
+for (int i=1; i<=n; i++)
+{
+    Console.Write($" {Math.Pow(i,3)},");
+}
+Console.Write("\b \b");
